2f9304b97175e38f589dc6bd4ecfd95d87884fc0
KohakuChan <69802296+MelidaZ@users.noreply.github.com>
2021-11-14 01:18:40 +0700

    Update Print.java

--- Print.java
+++ Print.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         char lol = love;
-        String output = "i" + zero + "u";
+        String output = "i" + lol + "u";
         System.out.println(output);
     }
 }
